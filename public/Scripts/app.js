4fba95d00b0b1135c8e61776166452c48fcaa318
JustHeroic <113134339+JustHeroic@users.noreply.github.com>
2022-10-30 19:54:37 -0400

    First Commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,17 @@
+const e = require("express");
+
+(function(){
+    function Start()
+    {
+        console.log("App Started");
+    }
+    window.addEventListener("load", Start);
+    var scrollLink = $(function(e) 
+      {
+        e.preventDefau1t();
+        $('body,html').animate({
+          scrollTop: $(this.hash).offset().top
+        },);
+      });
+
+})();
